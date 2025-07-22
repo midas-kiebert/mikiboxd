183774adccf2ee0a68728b2853fbf7cd2e7ca6b3
Midas Kiebert <midascubert@gmail.com>
2025-07-22 13:09:21 +0200

    fix indentation

--- backend/app/scheduler.py
+++ backend/app/scheduler.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     scheduler = BlockingScheduler()
     scheduler.add_job(
         func=scrape_data,
-        trigger=CronTrigger(hour=13, minute=10, timezone=ZoneInfo("Europe/Amsterdam")),
+        trigger=CronTrigger(hour=13, minute=15, timezone=ZoneInfo("Europe/Amsterdam")),
         id="nightly_scrape",
     )
     scheduler.start()

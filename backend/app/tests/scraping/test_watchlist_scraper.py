f162d9b60f53c3d6606ca31d8da065d3e68ca6ac
Midas Kiebert <midascubert@gmail.com>
2025-07-26 16:37:19 +0200

    fix tests

--- backend/app/tests/scraping/test_watchlist_scraper.py
+++ backend/app/tests/scraping/test_watchlist_scraper.py
@@ -1,3 +1,5 @@
+from unittest.mock import AsyncMock
+
 from bs4 import BeautifulSoup
 from pytest_mock import MockerFixture
 
@@ -10,14 +12,16 @@ def test_watchlist_parsing(
     sample_watchlist_empty_page: BeautifulSoup,
 ):
     # Mock the BeautifulSoup object to return the sample HTML
+    mock_get_page = AsyncMock(side_effect=[sample_watchlist_page_1, sample_watchlist_empty_page])
     mocker.patch(
-        "app.scraping.letterboxd.watchlist.get_page",
-        side_effect=[sample_watchlist_page_1, sample_watchlist_empty_page],
+        "app.scraping.letterboxd.watchlist.get_page_async", mock_get_page,
     )
 
     watchlist = get_watchlist("sample username")
 
     assert isinstance(watchlist, list), "Expected watchlist to be a list"
     assert len(watchlist) == 100, "Expected watchlist to contain 100 items"
-    assert watchlist[0] == "fail-safe"
-    assert watchlist[99] == "brief-encounter"
+    sorted_watchlist = sorted(watchlist)
+    assert sorted_watchlist[0] == "4-months-3-weeks-and-2-days"
+    assert sorted_watchlist[50] == "rebels-of-the-neon-god"
+    assert sorted_watchlist[99] == "yi-yi"

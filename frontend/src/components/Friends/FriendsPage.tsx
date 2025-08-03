03cfe036ee1545e28ade1adfdcd813651f97e3cf
Midas <91825386+midas-kiebert@users.noreply.github.com>
2025-08-04 00:10:03 +0200

    fix friends page (#73)
    * improved friends page
    
    * index on feature/friends-page: 226c143 improved friends page

--- frontend/src/components/Friends/FriendsPage.tsx
+++ frontend/src/components/Friends/FriendsPage.tsx
@@ -1,9 +1,12 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box, Grid } from "@chakra-ui/react";
 import Page from "@/components/Common/Page";
 import Sidebar from "@/components/Common/Sidebar";
 import FriendsTopBar from "@/components/Friends/FriendsTopBar";
 import { useState} from "react";
 import SearchUsers from "@/components/Friends/SearchUsers";
+import Friends from "@/components/Friends/Friends";
+import ReceivedRequests from "@/components/Friends/ReceivedRequests";
+import SentRequests from "@/components/Friends/SentRequests";
 
 const FriendsPage = () => {
     const [searchQuery, setSearchQuery] = useState<string>("");
@@ -18,7 +21,23 @@ const FriendsPage = () => {
                 />
             </Flex>
             <Page>
-                <SearchUsers query={searchQuery}/>
+                <Grid templateColumns="1fr 1fr" gap={4}>
+                    <Box
+                        // bg={"green.50"}
+                        minH={"calc(100vh - 64px)"}
+                        p={4}
+                    >
+                        <SearchUsers query={searchQuery}/>
+                    </Box>
+                    <Box
+                        // bg={"green.50"}
+                        p={4}
+                    >
+                        <ReceivedRequests/>
+                        <SentRequests/>
+                        <Friends/>
+                    </Box>
+                </Grid>
             </Page>
         </>
     );

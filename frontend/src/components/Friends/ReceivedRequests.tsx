d6927a36e760d12f75186d1f1bb72fbe80cc7e44
Midas Kiebert <midascubert@gmail.com>
2025-08-03 23:53:21 +0200

    improved friends page

---
+++ frontend/src/components/Friends/ReceivedRequests.tsx
@@ -0,0 +1,29 @@
+import { Box, Heading } from '@chakra-ui/react';
+import { useFetchReceivedRequests } from '@/hooks/useFetchReceivedRequests';
+import UserCard from './UserCard';
+
+
+const ReceivedRequests = () => {
+    const {
+        data : requests,
+    } = useFetchReceivedRequests();
+
+    return (
+        <Box>
+            { requests?.length! > 0 && (
+                <Heading as="h2" size="lg" mb={4} ml={2}>
+                    Requests Received
+                </Heading>
+                )
+            }
+            {requests?.map((user) => (
+                <UserCard
+                    key={user.id}
+                    user={user}
+                />
+            ))}
+        </Box>
+    );
+}
+
+export default ReceivedRequests;

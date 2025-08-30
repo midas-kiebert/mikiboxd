1daa9bd7997a810186a54a33a39099c5a20c80e1
Midas <91825386+midas-kiebert@users.noreply.github.com>
2025-08-31 00:55:05 +0200

    re-added the amsterdam scrapers as backup (and more up to date) (#101)

--- backend/app/scheduler.py
+++ backend/app/scheduler.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     scheduler = BlockingScheduler()
     scheduler.add_job(
         func=scrape_data,
-        trigger=CronTrigger(hour=0, minute=30, timezone=ZoneInfo("Europe/Amsterdam")),
+        trigger=CronTrigger(hour=1, minute=0, timezone=ZoneInfo("Europe/Amsterdam")),
         id="nightly_scrape",
     )
     scheduler.start()

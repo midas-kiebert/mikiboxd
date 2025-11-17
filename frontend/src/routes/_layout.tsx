a701991e473c318a95ec42b41dde926a1955b4e9
Midas Kiebert <midascubert@gmail.com>
2025-11-17 22:05:03 +0100

    stupid typescript fix

--- frontend/src/routes/_layout.tsx
+++ frontend/src/routes/_layout.tsx
@@ -1,7 +1,7 @@
 import { Flex } from "@chakra-ui/react"
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router"
 
-import Navbar from "@/components/Common/Navbar"
+// import Navbar from "@/components/Common/Navbar"
 import Sidebar from "@/components/Common/Sidebar"
 import { isLoggedIn } from "@/hooks/useAuth"
 import { Box } from "@chakra-ui/react"

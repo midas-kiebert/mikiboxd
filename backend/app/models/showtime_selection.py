e10a9fec315a5df378a971b7ab0638e5d13d293a
Midas Kiebert <midascubert@gmail.com>
2025-12-23 14:52:26 +0100

    added going status to showtime selection

--- backend/app/models/showtime_selection.py
+++ backend/app/models/showtime_selection.py
@@ -1,6 +1,9 @@
 from uuid import UUID
 
-from sqlmodel import Field, SQLModel
+from sqlalchemy import Enum as SAEnum
+from sqlmodel import Column, Field, SQLModel
+
+from app.core.enums import GoingStatus
 
 __all__ = [
     "ShowtimeSelection",
@@ -10,3 +13,7 @@ __all__ = [
 class ShowtimeSelection(SQLModel, table=True):
     user_id: UUID = Field(foreign_key="user.id", primary_key=True)
     showtime_id: int = Field(foreign_key="showtime.id", primary_key=True)
+    going_status: GoingStatus = Field(
+        default=GoingStatus.GOING,
+        sa_column=Column(SAEnum(GoingStatus, native_enum=False), nullable=False)
+    )
